5c4b1d49d053a55eb9b9a35fbd82f150fc88a146
NeeleshBShetty <neeleshshetty@1236@gmail.com>
2023-12-17 20:10:12 +0530

    Movie Detail Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import "./App.css"
 import AllRoutes from "./routes/AllRoutes"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import ScrollToTop from "./components/ScrollToTop"
 const App = () => {
   return (
     <div className="App dark:bg-slate-800">
+      <ScrollToTop />
       <Header />
       <AllRoutes />
       <Footer />
